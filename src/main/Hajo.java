6d4f643f3b9827cc318c3e5d2327cdae841892f2
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-05-23 15:15:31 +0200

    tesztLoves() atnevezve: tesztLovesTalalat(), teszt: sikeres

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -4,10 +4,16 @@ package main;
 
 public class Hajo {
     private int[] pozicio;
-    
-    
+
+    public Hajo(int[] pozicio) {
+        this.pozicio=pozicio;
+    }
     
     public String talalat(int poz){
-        return "";
+        int i=0;
+        while (i<this.pozicio.length && !(this.pozicio[i]==poz)){
+            i++;
+        }
+        return i<this.pozicio.length ? "talált":"mellé";
     }
 }
--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -5,11 +5,11 @@ package main;
 public class TorpedoTeszt {
     public static void main(String[] args) {
         System.out.println("Teszt");
-        new TorpedoTeszt().tesztLoves(4);
+        new TorpedoTeszt().tesztLovesTalalat(4);
     }
     
-    public String tesztLoves(int poz){
-        Hajo hajo = new Hajo();
+    public String tesztLovesTalalat(int poz){
+        Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
